6802f5a6d929cd1d4bf20163364092d66209fcad
Xupaya81 <christoferibarra793@gmail.com>
2024-10-29 18:18:54 -0300

    actualizacion

--- clases/empleados.py
+++ clases/empleados.py
@@ -1,5 +1,16 @@
 from datetime import datetime
-import Tipo
+from tipo_usuario import id_tipo
 
 class Empleados():
- def __init__(self, id_empleado, Nombre, fecha_contrato, fecha_nacimiento, salario, correo, telefono, direccion, id_tipo ):      
+ 
+ def __init__(self, id_empleado, Nombre_empleado, fecha_contrato, fecha_nacimiento, salario, correo, telefono, direccion, id_tipo, rut ):      
+  self. id_empleado = id_empleado,
+  self. Nombre_empleado = Nombre_empleado
+  self. fecha_contrato = fecha_contrato
+  self. fecha_nacimeinto = fecha_nacimiento
+  self. salario = salario 
+  self. correo = correo
+  self. telefono = telefono
+  self. direccion = direccion
+  self. id_tipo = id_tipo
+  self. rut = rut
