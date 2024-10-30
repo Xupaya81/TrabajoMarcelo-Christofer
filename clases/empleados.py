3e929815c874e20745a2eac57b0d44a7e13eee50
Xupaya81 <christoferibarra793@gmail.com>
2024-10-30 12:39:26 -0300

    actualizacion

--- clases/empleados.py
+++ clases/empleados.py
@@ -1,16 +1,28 @@
 from datetime import datetime
-from tipo_usuario import id_tipo
+from tipo_usuario import id_tipo_usuario
+from rut_chile import rut_chile
 
 class Empleados():
  
- def __init__(self, id_empleado, Nombre_empleado, fecha_contrato, fecha_nacimiento, salario, correo, telefono, direccion, id_tipo, rut ):      
-  self. id_empleado = id_empleado,
+ def __init__(self, id_empleado, Nombre_empleado, fecha_contrato, fecha_nacimiento, salario, correo, telefono, direccion, id_tipo_usuario, rut_empleado):      
+  super().__init__(id_tipo_usuario)
+  self. id_empleado = id_empleado
   self. Nombre_empleado = Nombre_empleado
   self. fecha_contrato = fecha_contrato
-  self. fecha_nacimeinto = fecha_nacimiento
+  self. fecha_nacimiento = fecha_nacimiento
   self. salario = salario 
   self. correo = correo
   self. telefono = telefono
   self. direccion = direccion
-  self. id_tipo = id_tipo
-  self. rut = rut
+  self. rut_empleado = rut_empleado
+
+  def validar_rut(self):
+        return rut_chile.is_valid_rut(self.rut_empleado)
+  
+  def validar_correo(self):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        if(re.fullmatch(regex, self.correo_usuario)):
+            return True 
+        else:
+            return False
+ 
