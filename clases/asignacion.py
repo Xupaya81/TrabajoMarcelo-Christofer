d0f55524457c9c421fb183584ff79a34d0da5f41
Xupaya81 <christoferibarra793@gmail.com>
2024-11-05 16:32:03 -0300

    actualizacion

--- clases/asignacion.py
+++ clases/asignacion.py
@@ -1,7 +1,8 @@
 from empleados import Empleados
 from departamento import departamento 
+
 class asignacion ():
     def __init__(self, id_asignacion, id_departamento, id_empleado):
-        self. id_asignacion = id_asignacion
-        self. id_departamento = id_departamento
-        self. id_empleado = id-id_empleado
+        self.id_asignacion = id_asignacion
+        self.id_departamento = id_departamento
+        self.id_empleado = id_empleado
--- clases/proyectoEmpleado.py
+++ clases/proyectoEmpleado.py
@@ -1,9 +1,10 @@
 from empleados import Empleados
 from proyecto import proyecto
+
 class proyecto_empleado():
     def __init__(self, id_proyecto_empleado, id_empleado, id_proyecto):
-        self. id_proyecto_empleado = id_proyecto_empleado
-        self. id_empleado= id_empleado
-        self. id_proyecto = id_proyecto
+        self.id_proyecto_empleado = id_proyecto_empleado
+        self.id_empleado= id_empleado
+        self.id_proyecto = id_proyecto
 
 
